2877c32c4607d0601ea0b66215dae75b433fa1eb
Got'Wasin <admin@Wasin-MacBook-Pro.local>
2019-09-02 14:33:41 +0700

    change for master

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"log"
 )
 
-const webContent = "Hello OpenLandscape on Production!"
+const webContent = "Hello OpenLandscape on Master!"
 
 func main() {
 	http.HandleFunc("/", helloHandler)
